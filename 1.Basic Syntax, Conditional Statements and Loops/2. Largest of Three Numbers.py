8647f04e1f8df9635e67441d1dad83fda106a0f5
Kiuliumov <miti.bania@gmail.com>
2023-12-16 21:57:43 +0200

    First Lecture

--- 1.Basic Syntax, Conditional Statements and Loops/2. Largest of Three Numbers.py
+++
@@ -1,9 +0,0 @@
-user_input1 = int(input())
-user_input2 = int(input())
-user_input3 = int(input())
-if user_input1 > user_input2 and user_input1 > user_input3:
-    print(user_input1)
-elif user_input2 > user_input1 and user_input2 > user_input3:
-    print(user_input2)
-elif user_input3 > user_input2 and user_input3 > user_input1:
-    print(user_input3)
