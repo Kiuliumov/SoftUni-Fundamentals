7661a54d9ff9bdd57b1575453064d37512f0c1c8
Kiuliumov <miti.bania@gmail.com>
2023-12-18 06:52:23 +0200

    SoftUni

---
+++ 8.Data Types and Variables/3. Pounds to Dollars.py
@@ -0,0 +1,3 @@
+pounds = int(input())
+dollars = pounds * 1.31
+print(f'{pounds:.2f}')
---
+++ 8.Data Types and Variables/4. Centuries to Minutes.py
@@ -0,0 +1,7 @@
+from math import floor
+centuries = int(input())
+years = round(centuries * 100)
+days = round(years * 365.2422)
+hours = round(days * 24)
+minutes = round(hours * 60)
+print(f'{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes')
