4578489742151ecad65d400b0172d46f0c40dddf
Kiuliumov <miti.bania@gmail.com>
2023-12-26 21:36:51 +0200

    SoftUni

--- 17. Lists Advanced/6. Even Numbers.py
+++ 17. Lists Advanced/6. Even Numbers.py
@@ -2,5 +2,8 @@ given_array = input()
 given_array = given_array.split(', ')
 even_indexes = []
 for i in given_array:
+    i = int(i)
     if i % 2 == 0:
-        even_indexes.append(given_array.index(i))
+        ind = given_array.index(str(i))
+        even_indexes.append(ind)
+print(even_indexes)
