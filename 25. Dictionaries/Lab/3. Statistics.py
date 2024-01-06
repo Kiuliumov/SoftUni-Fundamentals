e61cdcb5b83089f878f8451116a0275ec51473ea
Kiuliumov <miti.bania@gmail.com>
2024-01-06 09:37:03 +0200

    SoftUni

--- 25. Dictionaries/Lab/3. Statistics.py
+++ 25. Dictionaries/Lab/3. Statistics.py
@@ -3,10 +3,13 @@ products = {}
 while command != 'statistic':
     command_parts = input().split(': ')
     product = command_parts[0]
-    quantity = command_parts[1]
+    quantity = int(command_parts[1])
     if product not in products:
-        products[product] = int(quantity)
+        products[product] = 0
+    product[product] += quantity
     command = input()
 print('Products in stock:')
 for product, quantity in products.items():
     print(f'- {product}: {quantity}')
+print(f'Total Products: {len(products.keys())}')
+print(f'Total Quantity {sum(products.values())}')
