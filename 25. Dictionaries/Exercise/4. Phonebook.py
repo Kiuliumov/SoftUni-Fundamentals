fe8b4e61ebacf3a43dd25da0f45330097ad102c4
Kiuliumov <miti.bania@gmail.com>
2024-02-05 09:24:00 +0200

    Fundamentals

--- 25. Dictionaries/Exercise/4. Phonebook.py
+++ 25. Dictionaries/Exercise/4. Phonebook.py
@@ -1,12 +1,12 @@
 contact = input()
-contacts = {}
+contact_list = {}
 while not contact.isdigit():
     contact_info = contact.split('-')
-    contacts[contact_info[0]] = contact_info[1]
+    contact_list[contact_info[0]] = contact_info[1]
     contact = input()
 for _ in range(int(contact)):
     name = input()
-    if name in contacts:
-        print(f'{name} -> {contacts[name]}')
+    if name in contact_list:
+        print(f'{name} -> {contact_list[name]}')
     else:
         print(f"Contact {name} doesn't exist")
